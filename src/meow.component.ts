e337fd6a24405b8dfeae4bf7303f30f888519a95
Esteban Vega <esteban.veg4@gmail.com>
2016-12-25 21:57:57 +0100

    Add example module, component and entry file

---
+++ src/meow.component.ts
@@ -0,0 +1,10 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'meow',
+  templateUrl: './meow.component.html'
+})
+export class MeowComponent {
+}
---
+++ src/meow.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MeowComponent } from './meow.component';
+
+@NgModule({
+  declarations: [
+    MeowComponent
+  ]
+})
+export class MeowModule { }
