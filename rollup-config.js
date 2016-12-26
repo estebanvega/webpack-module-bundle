9d1f8f67a0ace3bf78b52e839d38ced1246c0cdd
Esteban Vega <esteban.veg4@gmail.com>
2016-12-26 17:37:56 +0100

    Add rollup config

---
+++ rollup-config.js
@@ -0,0 +1,19 @@
+import rollup      from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'index.js',
+  dest: 'dist/build.js',
+  sourceMap: false,
+  format: 'iife',
+  moduleName: 'meowBundle',
+  plugins: [
+      nodeResolve({jsnext: true, module: true}),
+      commonjs({
+        include: 'node_modules/rxjs/**',
+      }),
+      uglify()
+  ]
+}
